27a79bd47f68c51e29aa68903d9e9de5ce373fa3
Vorleak Yek <vorleakyek@gmail.com>
2024-02-09 23:02:23 +0000

    fix typescript errors.

--- client/src/components/AppContext.tsx
+++ client/src/components/AppContext.tsx
@@ -1,5 +1,6 @@
 import { createContext } from 'react';
 import type { Auth, User } from '../lib/api';
+import { FormEvent } from 'react';
 
 type AppContentValues = {
   user: User | undefined;
@@ -9,7 +10,7 @@ type AppContentValues = {
   star: number;
   handleSignIn: (auth: Auth) => void;
   handleSignOut: () => void;
-  handleLevelAndTheme: () => void;
+  handleLevelAndTheme: (event: FormEvent<HTMLFormElement>) => void;
 };
 
 export const AppContext = createContext<AppContentValues>({
